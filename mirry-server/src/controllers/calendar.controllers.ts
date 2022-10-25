34464d3ac125a0edde081aa69a167d4eff26581d
Ruslan Zaripov <zaripo.rus@gmail.com>
2022-10-25 13:24:41 +0200

    Add events functionality

--- mirry-server/src/controllers/calendar.controllers.ts
+++ mirry-server/src/controllers/calendar.controllers.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { getCalendarsListFromApi } from "../repositories/calendar.repository";
+import { getCalendarsListFromApi, getEventsListFromApi } from "../repositories/calendar.repository";
+import { getMonday, getSunday } from "../utils/calendar.utils";
 import { getInternalError } from "../utils/utils";
 
 const getCalendarsList = async (req: Request, res: Response, next: NextFunction) => {
@@ -17,6 +18,22 @@ const getCalendarsList = async (req: Request, res: Response, next: NextFunction)
     }
 }
 
+const getWeekEventsList = async (req: Request, res: Response, next: NextFunction) => {
+    const { user } = req.headers
+
+    try {
+        const calendarId: string = req.query.calendarId as string
+        const eventsResponse = (await getEventsListFromApi(calendarId, user as string, getMonday(), getSunday())).data
+        
+        return res
+            .status(StatusCodes.OK)
+            .json(eventsResponse.items)
+    } catch (e) {
+        return next(getInternalError(e))
+    }
+}
+
 export {
-    getCalendarsList
+    getCalendarsList,
+    getWeekEventsList
 }
--- mirry-server/src/routes/routes/calendar.routes.ts
+++ mirry-server/src/routes/routes/calendar.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getCalendarsList } from "../../controllers/calendar.controllers";
+import { getCalendarsList, getWeekEventsList } from "../../controllers/calendar.controllers";
 import authenticate from "../../middleware/jwtAuthentication.middleware";
+
 
 
 const calendarRouter = Router()
 
 calendarRouter.get('/list', authenticate, getCalendarsList)
+calendarRouter.get('/events', authenticate, getWeekEventsList)
 
 export default calendarRouter
